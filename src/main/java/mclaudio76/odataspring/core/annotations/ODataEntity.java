6702850904095e6bf92efcb63f4fb935da7283c7
MC <mauri.claudio@gmail.com>
2016-12-08 11:06:34 +0100

    Removed unused import

--- src/main/java/mclaudio76/odataspring/core/annotations/ODataEntity.java
+++ src/main/java/mclaudio76/odataspring/core/annotations/ODataEntity.java
@@ -6,7 +6,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import mclaudio76.odataspring.core.IODataService;
+
 
 @Retention(RUNTIME)
 @Target(TYPE)
